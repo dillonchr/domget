66e03cb4fe4640b09a620487eea151e450b40154
Dillon Christensen <j.dillon.christensen@gmail.com>
2019-10-11 21:30:18 -0500

    added more control to http request

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
-const http = require('http');
-const https = require('https');
+const fetch = require('@dillonchr/fetch');
 const {parse} = require('node-html-parser');
 
-module.exports = (url, callback) => {
-    const req = (url.includes('https://') ? https : http).get(url, (response) => {
-        let body = '';
-        response
-            .on('data', chunk => body += chunk)
-            .on('end', () => {
-                if (response.statusCode === 200) {
-                    try {
-                        const document = parse(body);
-                        callback(null, document);
-                    } catch (err) {
-                        callback(err);
-                    }
-                } else {
-                    callback(new Error(response.statusCode));
-                }
-            })
-            .on('error', callback);
+module.exports = (urlOrOpts, callback) => {
+    const opts = typeof urlOrOpts === 'string' ?
+        {url: urlOrOpts} :
+        urlOrOpts;
+    fetch(opts, (err, body) => {
+        if (err) {
+            return callback(err);
+        }
+        try {
+            const dom = parse(body);
+            callback(null, dom);
+        } catch(parseError) {
+            callback(parseError);
+        }
     });
 };
